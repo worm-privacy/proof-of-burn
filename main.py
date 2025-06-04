13708ca3ee06067bc013f5863521061c85956210
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-06-04 15:25:00 +0330

    Complete proof

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import web3
 
-MAX_LEN = 512
+MAX_LEN = 768
 MAX_LAYERS = 10
 
-layers = [[1, 2, 3], [4, 5, 6]]
+w3 = web3.Web3(provider=web3.Web3.HTTPProvider("https://rpc.payload.de/"))
+addr = "0x000000000000000000000000000000000000dEaD"
+
+blknum = w3.eth.block_number
+proof = w3.eth.get_proof(addr, [], blknum)
+state_root = w3.eth.get_block(blknum).stateRoot
+addr_hash = web3.Web3.keccak(hexstr=addr)[10:]
+leaf = proof.accountProof[-1]
+prefix = leaf[: leaf.index(addr_hash) + 22]
+
+extracted_layers = []
+for layer in list(reversed(proof.accountProof))[1:]:
+    extracted_layers.append(list(layer))
+
+balance = proof.balance
+
+leaf_prefix = list(prefix)
+leaf_prefex_size = len(leaf_prefix)
+leaf_prefix = leaf_prefix + [0] * (MAX_LEN - leaf_prefex_size)
+
+layers = extracted_layers
+num_layers = len(layers)
 layer_sizes = [len(l) for l in layers]
 
 for i in range(len(layers)):
@@ -20,8 +41,14 @@ def layer_to_str(layer):
 
 
 out = f"""
+leaf_prefix = {layer_to_str(leaf_prefix)}
+leaf_prefix_size = "{leaf_prefex_size}"
+balance = "{balance}"
+address = {layer_to_str(web3.Web3.to_bytes(hexstr=addr))}
 layers = [{', '.join([layer_to_str(l) for l in layers])}]
-layerSizes = [{', '.join(['"' + str(sz) + '"' for sz in layer_sizes])}]
+layer_sizes = [{', '.join(['"' + str(sz) + '"' for sz in layer_sizes])}]
+num_layers = "{num_layers}"
+// HASH: {state_root.hex()}
 """
 
 print(out)
