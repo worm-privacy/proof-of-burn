5f593d5e07f502d6f8f405be6e8fc73d37e49eef
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2025-06-11 12:36:06 +0330

    Remove Main.sol

--- Main.sol
+++
@@ -1,9 +0,0 @@
-contract Main {
-    constructor() {
-    }
-    function mint() public {
-        uint256 bal = 123;
-        uint256 nul = 234;
-        keccak256(abi.encodePacked(blockhash(block.number), bal, nul));
-    }
-}
